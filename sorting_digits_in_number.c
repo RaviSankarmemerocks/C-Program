0b9b639f802823b2e1426eb1d5979b91941bf7e9
Ravisankar Maru Ramesh <35621191+RaviSankarmemerocks@users.noreply.github.com>
2019-05-09 13:32:05 +0530

    Create sorting_digits_in_number.c
    zoho round 2

---
+++ sorting_digits_in_number.c
@@ -0,0 +1,27 @@
+@author ravisankar
+#include <stdio.h>
+void main () {
+ long n,digit,temp,new=0;
+ int pow=1;
+ n = 637206380746;
+ for (digit=0;digit<=9;digit++){
+   for(temp=n;temp>0;temp/=10){
+     
+     if (temp%10>0&&temp%10==digit){
+     
+      new=new*10+digit;
+
+  }if(temp%10==digit&&temp%10==0){
+  	printf("%ld",digit);
+    pow=pow*10;
+
+  }
+  
+ 
+}
+}
+digit=0;
+temp=0;
+ //printf("\n");
+ printf("%ld\n",new);
+ }
