2488d86778540ba83d52e0c547c2e0afacc68acd
Ravisankar Maru Ramesh <35621191+RaviSankarmemerocks@users.noreply.github.com>
2019-06-12 01:31:48 +0530

    Update reverse_sentense.c

--- reverse_sentense.c
+++ reverse_sentense.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
-#include<string.h>
+
 int main()
 {
- /*   char name[20];
-    printf("Enter name: ");
-    scanf("%s", name);
-    printf("Your name is %s.", name);*/
+
     char name1[1000];
     printf("Enter name: ");
     fgets(name1,100000,stdin);     // read string
     //printf("Name: ");
     //puts(name1);    // display string
-    int temp=strlen(name1)-1;//sizeof(name1)/sizeof(char);
+int temp=0;
+    
+    for(int x=0;x<sizeof(name1)/sizeof(char);x++)
+    {
+    	int ascii=name1[x];
+    	if(isalpha(name1[x])||ascii==32){
+     
+    		temp=temp+1;
+    }
+    else{
+    	break;
+    }
+    }
+
+
+
+
+
+
+
+
+
+    //int temp=strlen(name1)-1;//sizeof(name1)/sizeof(char);
     printf("%d\n",temp);
         for (int i = temp-1; i>=0; i--) {
             if(name1[i]==' '){
